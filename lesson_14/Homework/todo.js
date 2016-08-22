4351744eb9c69ad3fdc2148438193c95ace85cc5
evandersar <evandersar@ukr.net>
2016-08-22 21:08:14 +0300

    class and home tasks for 14th lesson are done

---
+++ lesson_14/Homework/todo.js
@@ -0,0 +1,15 @@
+var model = {
+            tasks: [
+
+            { name: "Eat lunch",
+            description: 'Eat delicious lanch',
+            date: '15.08.2016',
+            completed: true },
+
+            { name: "Buy food",
+            description: 'Buy food in market',
+            date: '15.08.2016',
+            completed: true }
+
+            ]
+        };
